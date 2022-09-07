038017e2251947f523226f5cbe65c4e34b9117d6
Eduard Aymerich <eduardaymerich@gmail.com>
2022-09-08 01:18:01 +0200

    add logos

---
+++ components/Logos.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container justify-center text-center text-xl my-16 md:my-12 mx-auto">
+        <h1>Trusted by</h1>
+        <div class="grid grid-cols-3 md:grid-cols-6 gap-1 md:gap-4 items-center px-4 md:px-0 mt-4 md:mt-2 ">
+            <img src="assets/img/fundacion_vodafone.png" class="grayscale hover:grayscale-0" />
+            <img src="assets/img/explorer.png" class="grayscale hover:grayscale-0" />
+            <img src="assets/img/zurich.svg" class="grayscale hover:grayscale-0" />
+            <img src="assets/img/cruz_roja.png" class="grayscale hover:grayscale-0" />
+            <img src="assets/img/actuaupm.jpeg" class="grayscale hover:grayscale-0" />
+            <img src="assets/img/fpdgi.jpeg" class="grayscale hover:grayscale-0" />
+        </div>
+    </div>
+</template>
