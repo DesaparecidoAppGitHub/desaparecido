0b03cfeeafac4ea7801fdc82fe1ce21e6cc59953
Eduard Aymerich <eduardaymerich@gmail.com>
2023-01-26 03:31:50 +0100

    add links to transparency and updates

--- components/Updates.vue
+++ components/Updates.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="text-center p-8 md:py-16 bg-monochrome-100">
-        <div class="md:w-1/2 mx-auto">
+    <div class="p-8 text-center md:py-16 bg-monochrome-100">
+        <div class="mx-auto md:w-1/2">
             <h1 class="text-4xl font-bold leading-[3.5rem] text-monochrome-800">
                 {{ $t("updates-title") }}
             </h1>
-            <p class="text-lg w-full leading-relaxed mt-8 text-monochrome-600 mb-16">
+            <p class="w-full mt-8 mb-16 text-lg leading-relaxed text-monochrome-600">
                 {{ $t("updates-subtitle") }}
             </p>
-            <div class="text-lg leading-relaxed mt-8 text-secondary-400 border border-secondary-400 mb-16 py-2 px-2 rounded-full w-48 mx-auto">
-                {{ $t("coming-soon") }}
-            </div>
+            <a
+                href="https://desaparecido.notion.site/57d8494096344420bc002e2024da113f?v=2c4dba1f50d544b8b34449a9624d09d5"
+                target="_blank"
+                class="px-8 py-4 mb-8 text-sm rounded-full cursor-pointer bg-primary-300 hover:bg-primary-400 text-monochrome-50 md:mb-16"
+            >
+                {{ $t("updates-see") }}
+            </a>
         </div>
     </div>
 </template>
